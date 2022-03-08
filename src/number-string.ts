bdf67b14426031439a68fdf3c352990a76a5b809
AkiraHonda <05410kira@gmail.com>
2022-03-09 00:49:25 +0900

    boolean,number,string,array型

---
+++ src/number-string.ts
@@ -0,0 +1,15 @@
+export {};
+
+let year: number = 1976;
+// year = false;
+
+console.log(year);
+
+let age: number = 0x2b;
+
+console.log(age);
+
+// 型推論　let name: string
+let name = 'Ham';
+// name = 10;
+console.log(name);
